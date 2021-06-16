b00ab8f618e7dadc7deb42498ba3a1723e12bb6d
mnederkorn <36685815+mnederkorn@users.noreply.github.com>
2021-06-16 14:50:59 +0200

    added energy games; fix point iteration for dpg

--- grest.py
+++ grest.py
@@ -42,6 +42,19 @@ class MeanPayoffGame:
 
         return DiscountedMeanPayoffGame(self.vertices, self.owner, self.edges, self.threshold, discount)
 
+    def to_eg(self):
+
+        return EnergyGame(self.vertices, self.owner, self.edges, 0)
+
+class EnergyGame:
+
+    def __init__(self, vertices, owner, edges, credit):
+
+        self.vertices = vertices
+        self.owner = owner        
+        self.edges = edges
+        self.credit = credit
+
 class DiscountedMeanPayoffGame:
 
     def __init__(self, vertices, owner, edges, threshold, discount):
@@ -88,6 +101,28 @@ class DiscountedMeanPayoffGame:
 
         return SimpleStochasticGame(vertices, owner, ssg_edges, avg_chance)
 
+    def solve(self):
+
+        mini = np.iinfo(self.edges.dtype).min
+
+        cur = np.zeros(self.vertices).reshape((-1,1))
+        for _ in range(100*2):
+
+            old = np.array(cur)
+
+            edges_weight = np.where(self.edges != mini, ((1-self.discount)*self.edges)+self.discount*cur, np.nan)
+
+            cur = np.where(self.owner, np.nanmin(edges_weight, 1), np.nanmax(edges_weight, 1))
+
+            err = cur-old
+
+            print(cur)
+
+            # mi = np.amin(err)
+            # ma = np.amax(err)
+
+            # print(mi,np.frexp(mi)) if -mi>ma else print(ma,np.frexp(ma))
+
 class SimpleStochasticGame:
 
     def __init__(self, vertices, owner, edges, avg_chance):
@@ -99,32 +134,43 @@ class SimpleStochasticGame:
 
 if __name__ == '__main__':
 
-    N = 10
+    N = 20
 
-    # pg demo
-    vertices = N
-    owner = np.random.choice([False, True], size=(vertices))
-    edges = np.random.choice([False, True], size=(vertices,vertices))
-    priority = np.random.randint(0, vertices, size=(vertices))
-    pg = ParityGame(vertices,owner,edges,priority)
-    print(pg)
+    # # pg demo
+    # vertices = N
+    # owner = np.random.choice([False, True], size=(vertices))
+    # edges = np.random.choice([False, True], size=(vertices,vertices))
+    # priority = np.random.randint(0, vertices, size=(vertices))
+    # pg = ParityGame(vertices,owner,edges,priority)
+    # print(pg)
+    # mpg = pg.to_mpg()
+    # print(mpg)
 
-    mpg = pg.to_mpg()
-    print(mpg)
+    # # mpg demo
+    # vertices = N
+    # owner = np.random.choice([False, True], size=(vertices))
+    # edges_exist = np.random.choice([False, True], size=(vertices,vertices))
+    # edges_value = np.random.randint(-10, 11, size=(vertices,vertices))
+    # mini = np.iinfo(edges_value.dtype).min
+    # edges = np.where(edges_exist, edges_value, mini)
+    # threshold = np.random.randint(-vertices,vertices, size=(1))
+    # mpg = MeanPayoffGame(vertices, owner, edges, threshold)
+    # print(mpg)
+    # dpg = mpg.to_dpg()
+    # print(dpg)
+    # eg = mpg.to_eg()
+    # print(eg)
 
-    # mpg demo
-    vertices = N
-    owner = np.random.choice([False, True], size=(vertices))
-    edges_exist = np.random.choice([False, True], size=(vertices,vertices))
-    edges_value = np.random.randint(-10, 11, size=(vertices,vertices))
-    mini = np.iinfo(edges_value.dtype).min
-    edges = np.where(edges_exist, edges_value, mini)
-    threshold = np.random.randint(-vertices,vertices, size=(1))
-    mpg = MeanPayoffGame(vertices, owner, edges, threshold)
-    print(mpg)
-
-    dpg = mpg.to_dpg()
-    print(dpg)
+    # # eg demo
+    # vertices = N
+    # owner = np.random.choice([False, True], size=(vertices))
+    # edges_exist = np.random.choice([False, True], size=(vertices,vertices))
+    # edges_value = np.random.randint(-10, 11, size=(vertices,vertices))
+    # mini = np.iinfo(edges_value.dtype).min
+    # edges = np.where(edges_exist, edges_value, mini)
+    # credit = np.random.randint(-vertices*10,vertices*10, size=(1))
+    # eg = EnergyGame(vertices, owner, edges, credit)
+    # print(eg)
 
     # dpg demo
     vertices = N
@@ -137,31 +183,16 @@ if __name__ == '__main__':
     discount = np.random.rand(1)
     dpg = DiscountedMeanPayoffGame(vertices, owner, edges, threshold, discount)
     print(dpg)
+    dpg.solve()
+    # ssg = dpg.to_ssg()
+    # print(ssg)
 
-    ssg = dpg.to_ssg()
-    print(ssg)
-
-    # ssg demo
-    vertices = N
-    owner = np.random.randint(0, 3, size=(vertices), dtype=np.int8)
-    edges = np.random.choice([False, True], size=(vertices,vertices+2))
-    avg_n = len(np.where(owner==2)[0])
-    avg_chance = np.random.randint(0, 256, size=(avg_n,vertices+2), dtype=np.uint8)
-    avg_chance = np.where(edges[np.where(owner==2)], avg_chance, 0)
-    ssg = SimpleStochasticGame(vertices, owner, edges, avg_chance)
-    print(ssg)
-
-    # d = {False:"P0",True:"P1"}
-    # d2 = {False:"square",True:"circle"}
-
-    # view = Digraph(format="png")
-
-    # for v in range(vertices):
-    #     view.node(str(v), d[owner[v]], shape=d2[owner[v]])
-    # print(edges_value.shape)
-    # for i in range(edges_value.shape[0]):
-    #     for j in range(edges_value.shape[0]):
-    #         if edges_exist[i,j]:
-    #             view.edge(str(i),str(j),str(edges_value[i,j]))
-
-    # view.render(filename=r"C:\Users\Maxime\Desktop\test.png", view=True, cleanup=True)
+    # # ssg demo
+    # vertices = N
+    # owner = np.random.randint(0, 3, size=(vertices), dtype=np.int8)
+    # edges = np.random.choice([False, True], size=(vertices,vertices+2))
+    # avg_n = len(np.where(owner==2)[0])
+    # avg_chance = np.random.randint(0, 256, size=(avg_n,vertices+2), dtype=np.uint8)
+    # avg_chance = np.where(edges[np.where(owner==2)], avg_chance, 0)
+    # ssg = SimpleStochasticGame(vertices, owner, edges, avg_chance)
+    # print(ssg)
